3986338b07eec3c6c51d56a8d80378f4578e41d4
Shailseen <shailesh13246@gmail.com>
2021-09-29 11:31:12 +0530

    feat error handling

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 var butn = document.querySelector("#bttn");
 var txttaken = document.querySelector("#text");
 var txtoutput = document.querySelector("#txtinput-two");
+var messageBox = document.querySelector("#output-box");
 var URLlink = "https://api.funtranslations.com/translate/minion.json"
 // var URLlink = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-console.log("hey");
+// console.log("hey");
 
 function translatorURL(text) {
     return URLlink + "?" + "text=" + text
 }
 function errorHandler(error) {
-    console.log("Error !", error);
-    alert("Server down! Please try again");
+    messageBox.innerText = error.message;
+    // console.log(error.message);
+    // console.log(error);
+    // alert("Server down! Please try again");
+    // alert(error.message);
 }
 function clickHandler() {
     var inputText = txttaken.value;
     fetch(translatorURL(inputText))
         .then(response => response.json())
         .then(json => {
+            if(json.error){
+                return errorHandler(json.error);111111
+            }
             // console.log("here");
             var translatedText = json.contents.translated;
             // console.log(translatedText);
             txtoutput.textContent = (translatedText);
         })
-        .catch(errorHandler)
+        .catch(errorHandler);
     // var txt = txttaken.value;
     // console.log("clicked", txt);
-};
+}
 butn.addEventListener("click", clickHandler)
